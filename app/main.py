b8f1a3b6a792e64ec8206aac6123548a31d3b210
Sylvain <sylvainlecornu@gmail.com>
2023-03-27 22:54:14 +0200

    adding version to project

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
+from _version import __version__
 from opyoid import Injector
 from modules.module import AppModule
 from ipApp import IpApp
-
+print(f'Running HomeServerIpUpdates version {__version__}')
 injector=Injector([AppModule])
 app=injector.inject(IpApp)
 app.run()
